8a8ee6e8e121368a68b30664304ec2cc81c914e3
vinh huynh <vvvhuynh@yahoo.com>
2018-04-06 01:42:25 -0500

    added User & UserManager entities

---
+++ tacoblog/src/java/edu/acc/vinh/tacoblog/UserManager.java
@@ -0,0 +1,24 @@
+package edu.acc.vinh.tacoblog;
+
+import java.util.ArrayList;
+
+public class UserManager {
+    
+    ArrayList<User> userList = new ArrayList<>();
+    
+    // if there was an entry, iterate through list to see if user exists
+    public User findUserIfValid(String name, String password) {
+        if (name != null && password != null) {
+            for (User user : userList) {
+                if (user.getName().equals(name) && user.getPassword().equals(password)) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public void addUser(User newUser) {
+        userList.add(newUser);
+    }
+}
