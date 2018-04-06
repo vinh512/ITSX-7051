2710a0129e356a920f0c92abb35a41d53d60f25f
vinh huynh <vvvhuynh@yahoo.com>
2018-04-06 02:46:05 -0500

    add servlet listener to initialize data

---
+++ tacoblog/src/java/edu/acc/vinh/tacoblog/SetupApplication.java
@@ -0,0 +1,32 @@
+package edu.acc.vinh.tacoblog;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class SetupApplication implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // instantiate manager objects
+        BlogManager blogManager = new BlogManager();
+        UserManager userManager = new UserManager();
+        
+        // initialize app with blog data
+        blogManager.addBlog(new Blog("Torchy's", "Damn Good Tacos is their motto and its easy to see why."));
+        blogManager.addBlog(new Blog("Taco Deli", "Handcrafted gourmet tacos. Eat Great. Live Great."));
+        blogManager.addBlog(new Blog("Cool Beans", "This vegan Mexican spot is as good as anything fried in lard."));
+        
+        // initialize app with user data
+        userManager.addUser(new User("Bob", "password123"));
+        userManager.addUser(new User("Sally", "applepie"));
+        userManager.addUser(new User("Rufus", "monkey007"));
+        
+        // add manager objects to servlet context
+        sce.getServletContext().setAttribute("userManager", userManager);
+        sce.getServletContext().setAttribute("blogManager", blogManager);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {    
+    }
+}
--- tacoblog/src/java/edu/acc/vinh/tacoblog/TacoBlogServlet.java
+++ tacoblog/src/java/edu/acc/vinh/tacoblog/TacoBlogServlet.java
@@ -1,18 +1,30 @@
 package edu.acc.vinh.tacoblog;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "TacoBlogServlet", urlPatterns = {"/TacoBlogServlet"})
+@WebServlet("/TacoBlogServlet")
 public class TacoBlogServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        BlogManager blogManager = (BlogManager) getServletContext().getAttribute("blogManager");
+
+        // now that i have the blogmanger, i can use the method to get the array.
+        // then pass the array into the request for the jsp to use
+        ArrayList<Blog> blogList = blogManager.getAllBlogs();
+        
+        // pass in blog list into request object
+        request.setAttribute("blogList", blogList);
+        
+        
         request.getRequestDispatcher("/WEB-INF/displayblog.jsp").forward(request, response);
     }
 
