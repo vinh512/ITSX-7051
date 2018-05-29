afc8ff13a665f3ad6cd9ae51f962eb2f0625266d
vinh huynh <vvvhuynh@yahoo.com>
2018-05-28 23:33:14 -0500

    import User class

---
+++ neighborhoodpet/src/java/edu/acc/vinh/neighborhoodpet/User.java
@@ -0,0 +1,73 @@
+package edu.acc.vinh.neighborhoodpet;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;    
+    private int zipCode;
+    private String email;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
