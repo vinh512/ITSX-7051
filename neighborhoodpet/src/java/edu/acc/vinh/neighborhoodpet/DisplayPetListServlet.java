ef17b2514ba279266727d76a9f2ff1fd8c351e42
vinh huynh <vvvhuynh@yahoo.com>
2018-06-01 01:17:32 -0500

    set petList as attribute to petprofilelist jps

--- neighborhoodpet/src/java/edu/acc/vinh/neighborhoodpet/DisplayPetListServlet.java
+++ neighborhoodpet/src/java/edu/acc/vinh/neighborhoodpet/DisplayPetListServlet.java
@@ -1,22 +1,28 @@
 package edu.acc.vinh.neighborhoodpet;
 
 import java.io.IOException;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 @WebServlet("/DisplayPetListServlet")
 public class DisplayPetListServlet extends HttpServlet {
+    
+    @Resource(lookup = "java:app/jdbc/petsDB")
+    DataSource dataSource;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         System.out.println("*** Entered doGet on /DisplayPetListServlet! ***");
         
-//        PetManager petManager = (PetManager)request.getServletContext().getAttribute('petManager');
-        
+        PetManager petManager = (PetManager)request.getServletContext().getAttribute("petManager");
+                
+        request.setAttribute("petList", petManager.getPets(dataSource));
         request.getRequestDispatcher("/WEB-INF/petprofilelist.jsp").forward(request, response);
 
     }
