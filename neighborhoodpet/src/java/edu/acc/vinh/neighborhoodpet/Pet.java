aee4025e50a152f1cc48b9297489902c3f59d5f7
vinh huynh <vvvhuynh@yahoo.com>
2018-05-31 21:12:33 -0500

    add pet class

---
+++ neighborhoodpet/src/java/edu/acc/vinh/neighborhoodpet/Pet.java
@@ -0,0 +1,85 @@
+package edu.acc.vinh.neighborhoodpet;
+
+public class Pet {
+    private String petName;
+    private String breed;
+    private String gender;
+    private int age;
+    private String size;
+    private String color;
+    private boolean isMissing;
+    private String ownerID;
+    private String bio;
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isIsMissing() {
+        return isMissing;
+    }
+
+    public void setIsMissing(boolean isMissing) {
+        this.isMissing = isMissing;
+    }
+
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
